e52c1106feda55398994d20a45125e979db44f1e
isx45128227 <35956939+isx45128227@users.noreply.github.com>
2018-04-20 07:07:05 +0200

    Update twitterhashtags.sql

--- Postgres/twitterhashtags.sql
+++ Postgres/twitterhashtags.sql
@@ -125,6 +125,9 @@ ALTER TABLE tweets
 ADD CONSTRAINT fk_usuari
 FOREIGN KEY (id_usuari) REFERENCES usuaris(id_usuari);
 
+ALTER TABLE tweets 
+ADD CONSTRAINT fk_foto
+FOREIGN KEY (id_foto) REFERENCES fotos(id_foto);
 
 ALTER TABLE hashtagstweets 
 ADD CONSTRAINT fk_tweet
