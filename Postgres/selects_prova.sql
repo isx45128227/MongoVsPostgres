0d85aa2f6f4dc57d62870cac0095aa4f6014bc3e
isx45128227 <roogeer96@gmail.com>
2018-04-25 10:19:56 +0200

    EXPLAIN SELECT amb i sense indexs

--- Postgres/selects_prova.sql
+++ Postgres/selects_prova.sql
@@ -8,5 +8,46 @@ twitter=# SELECT * FROM tweets JOIN usuaris ON tweets.id_usuari=usuaris.id_usuar
 
 
 twitter=# UPDATE tweets SET text_tweet = text_tweet || ' que la seva ocupacio ha de ser com a maxim 280 caracters per comparar Mongo amb Postgres.';
+
+
 twitter=# SELECT tweets.text_tweet FROM tweets JOIN usuaris ON tweets.id_usuari=usuaris.id_usuari JOIN hashtagstweets ON tweets.id_tweet=hashtagstweets.id_tweet WHERE text_tweet LIKE '%#chip%' ORDER BY usuaris.telefon;
 twitter=# SELECT tweets.text_tweet FROM tweets JOIN usuaris ON tweets.id_usuari=usuaris.id_usuari JOIN hashtagstweets ON tweets.id_tweet=hashtagstweets.id_tweet WHERE text_tweet LIKE '%maxim%' ORDER BY usuaris.telefon;
+
+twitter=# EXPLAIN SELECT tweets.text_tweet FROM tweets JOIN usuaris ON tweets.id_usuari=usuaris.id_usuari JOIN hashtagstweets ON tweets.id_tweet=hashtagstweets.id_tweet WHERE text_tweet LIKE '%maxim%' ORDER BY usuaris.telefon;
+                                               QUERY PLAN                                                
+---------------------------------------------------------------------------------------------------------
+ Sort  (cost=431843.91..433093.79 rows=499949 width=213)
+   Sort Key: usuaris.telefon
+   ->  Merge Join  (cost=62353.91..281991.19 rows=499949 width=213)
+         Merge Cond: (tweets.id_tweet = hashtagstweets.id_tweet)
+         ->  Nested Loop  (cost=0.85..1299973.15 rows=379429 width=221)
+               ->  Index Scan using tweets_pkey on tweets  (cost=0.42..1011553.53 rows=379429 width=219)
+                     Filter: ((text_tweet)::text ~~ '%maxim%'::text)
+               ->  Index Scan using usuaris_pkey on usuaris  (cost=0.42..0.75 rows=1 width=18)
+                     Index Cond: (id_usuari = tweets.id_usuari)
+         ->  Materialize  (cost=62352.81..64852.80 rows=499999 width=8)
+               ->  Sort  (cost=62352.81..63602.81 rows=499999 width=8)
+                     Sort Key: hashtagstweets.id_tweet
+                     ->  Seq Scan on hashtagstweets  (cost=0.00..8184.99 rows=499999 width=8)
+(13 rows)
+
+
+twitterindexs=# EXPLAIN SELECT tweets.text_tweet FROM tweets JOIN usuaris ON tweets.id_usuari=usuaris.id_usuari JOIN hashtagstweets ON tweets.id_tweet=hashtagstweets.id_tweet WHERE text_tweet LIKE '%maxim%' ORDER BY usuaris.telefon;
+                                               QUERY PLAN                                               
+--------------------------------------------------------------------------------------------------------
+ Sort  (cost=437338.14..438588.01 rows=499949 width=213)
+   Sort Key: usuaris.telefon
+   ->  Hash Join  (cost=117150.47..287485.41 rows=499949 width=213)
+         Hash Cond: (tweets.id_usuari = usuaris.id_usuari)
+         ->  Merge Join  (cost=62353.49..190660.14 rows=499949 width=211)
+               Merge Cond: (tweets.id_tweet = hashtagstweets.id_tweet)
+               ->  Index Scan using tweets_idx on tweets  (cost=0.43..699763.00 rows=5999400 width=219)
+                     Filter: ((text_tweet)::text ~~ '%maxim%'::text)
+               ->  Materialize  (cost=62352.81..64852.80 rows=499999 width=8)
+                     ->  Sort  (cost=62352.81..63602.81 rows=499999 width=8)
+                           Sort Key: hashtagstweets.id_tweet
+                           ->  Seq Scan on hashtagstweets  (cost=0.00..8184.99 rows=499999 width=8)
+         ->  Hash  (cost=36436.99..36436.99 rows=999999 width=18)
+               ->  Seq Scan on usuaris  (cost=0.00..36436.99 rows=999999 width=18)
+(14 rows)
+
