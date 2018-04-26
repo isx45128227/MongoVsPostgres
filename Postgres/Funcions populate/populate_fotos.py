3d1d11036977ce761a4e9b37dcedc078fd467320
isx45128227 <roogeer96@gmail.com>
2018-04-26 11:27:29 +0200

    Rename funcions

---
+++ Postgres/Funcions populate/populate_fotos.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+#-*- coding: utf-8-*-
+
+import random, datetime
+
+now = datetime.datetime.now()
+
+## Funció que crea fotos
+for i in range (0,50000):
+	id_foto = random.randrange(1,49000,1)
+	id_tweet = random.randrange(1,100000,1)
+	text = "Foto %d molt xula!" %(i)
+	data = now.strftime("%Y-%m-%d %H:%M")
+	print "%d,%s,%d,%s"%(i,text,id_tweet,data)
+
+
+# python funciopopulate_fotos.py > fotos.csv
+# COPY fotos(id_foto,text_foto,id_tweet,data_foto) FROM '/tmp/fotos.csv' DELIMITER ',' CSV HEADER;
