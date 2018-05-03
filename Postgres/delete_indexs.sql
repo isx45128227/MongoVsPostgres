b6904a301cff2a25e8c1baf10d8c8a0798f31906
isx45128227 <roogeer96@gmail.com>
2018-05-03 13:38:00 +0200

    Delete indexs scripts

---
+++ Postgres/delete_indexs.sql
@@ -0,0 +1,31 @@
+-- DELETE indexs
+
+DROP INDEX id_usuari_tweets_idx;
+
+DROP INDEX id_foto_tweets_idx;
+
+DROP INDEX id_usuari_comentari_idx;
+
+DROP INDEX id_tweet_idx;
+
+DROP INDEX id_usuari_comentari_likes_idx;
+
+DROP INDEX id_tweet_likes_idx;
+
+DROP INDEX id_usuari_usuarislikescomentaris_idx;
+
+DROP INDEX id_comentari_usuarislikescomentaris_idx;
+
+DROP INDEX id_tweet_fotos_idx;
+
+DROP INDEX id_usuari_retweets_idx;
+
+DROP INDEX id_tweet_retweets_idx;
+
+DROP INDEX id_tweet_hashtagtweets_idx;
+
+DROP INDEX id_hashtag_hashtagtweets_idx;
+
+DROP INDEX id_usuariseguit_idx;
+
+DROP INDEX id_usuariseguidor_idx;
