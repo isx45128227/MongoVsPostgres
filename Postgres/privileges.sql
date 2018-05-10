9aa42f72e4748f5ca90b96a6ae355bd83ac21835
isx45128227 <roogeer96@gmail.com>
2018-05-10 11:09:21 +0200

    Script privilegis

---
+++ Postgres/privileges.sql
@@ -0,0 +1,17 @@
+-- PRIVILEGIS a la BBDD twitter per a poder fer consultes executant: psql -p 5432 -U username -d twitter
+
+CREATE USER YourUsername WITH PASSWORD 'password';
+
+
+GRANT ALL ON DATABASE twitter TO YourUsername;
+
+GRANT ALL ON usuaris TO YourUsername;
+GRANT ALL ON tweets TO YourUsername;
+GRANT ALL ON retweets TO YourUsername;
+GRANT ALL ON hashtagstweets TO YourUsername;
+GRANT ALL ON hashtags TO YourUsername;
+GRANT ALL ON likes TO YourUsername;
+GRANT ALL ON comentaris TO YourUsername;
+GRANT ALL ON seguidors TO YourUsername;
+GRANT ALL ON usuarislikescomentaris TO YourUsername;
+GRANT ALL ON fotos TO YourUsername;
