775597d2dad19382c920dc5026fb7022ac8630fe
isx45128227 <roogeer96@gmail.com>
2018-05-10 13:31:41 +0200

    Atac postgres canvi

--- Postgres/Atac/atac.sh
+++ Postgres/Atac/atac.sh
@@ -14,3 +14,7 @@ do
     ./execucio_script_atac.sh & 2> /dev/null
     i=$((i+1))
 done
+
+# Per comprovar el log de Postgres podem buscar al fitxer de logs i veure la duracio dels queries:
+# grep "ms  statement: SELECT" /var/lib/pgsql/data/pg_log/postgresql-QUERIES.log
+
