ff4a90203e9b7dde3b685382a1cd20ecd29c5b13
isx45128227 <roogeer96@gmail.com>
2018-05-15 12:59:33 +0200

    Queries de prova

--- MongoDB/queries_prova.js
+++ MongoDB/queries_prova.js
@@ -1,22 +1,34 @@
-// QUERIES PROVA
+// QUERIES PROVA MONGODB
 
+// Mostrar informació d'un usuari
 
-// Find usuaris a qui segueix
 var usuari = 1;
+db.users.find({"id_usuari": usuari},{"_id":0,"id_usuari":1,"nom":1,"cognoms":1,"descripcio":1,"ciutat":1,"email":1})
 
+
+// Tweet que tingui el text 5000
+db.tweets.find({"text_tweet":/5000/})
+
+
+// Tweet que acabi amb el text 5000
+db.tweets.find({"text_tweet":/5000$/})
+
+
+// Tweet que comenci amb el text 5000
+db.tweets.find({"text_tweet":/5000$/})
+
+
+// Usuaris a qui segueix
+var usuari = 1;
 db.users.find({"seguidors.id_usuari_seguidor": usuari},{"_id":0,"id_usuari":1})
 
 
-// Find num usuaris que segueix
+// Número usuaris que segueix
 var usuari = 1;
 db.users.find({"seguidors.id_usuari_seguidor": usuari},{"_id":0,"id_usuari":1}).count()
 
 
-// Find tweet que tingui el text 5000
-db.tweets.find({$text:{$search:"5000"}})
-
-
-// Find num usuaris que segueixen a cada usuari (GROUP BY)
+// Número usuaris que segueixen a cada usuari (GROUP BY)
 db.users.aggregate([
 					  {
 						  "$group": {
@@ -34,8 +46,7 @@ db.users.aggregate([
 ])
 
 
-
-// Find id_usuari que ha escrit un tweet que conté #chip
+// id_usuari que ha escrit un tweet que conté #chip
 function atac(query) {
 
   var data_inicial = new Date();
@@ -66,20 +77,23 @@ atac({"text_tweet":/#chip/},{"id_usuari":1});
 atac({$text:{$search:"#chip"}},{"id_usuari":1})
 
 
-
-// Número de likes de cada usuari
+// Número màxim, mínim i mitjana de likes de cada usuari
 db.tweets.aggregate([
 					  {
 						  "$group": {
 							   "_id": "$id_usuari",
-							   "num_likes": {"$sum": 1}
+							   "max_num_likes": {"$max": "$num_likes"},
+							   "min_num_likes": {"$min": "$num_likes"},
+							   "avg_num_likes": {"$avg": "$num_likes"}
 						   }
 					  },
 					  {
 						  "$project": {
 							   "_id":false,
 							   "id_usuari": "$_id",
-							   "num_likes":"$num_likes"
+							   "max_num_likes":"$max_num_likes",
+							   "min_num_likes":"$min_num_likes",
+							   "avg_num_likes":"$avg_num_likes"
 						   }
 					  },
 					  {
@@ -88,6 +102,27 @@ db.tweets.aggregate([
 ])
 
 
+// Comentaris que té cada usuari 
+db.tweets.aggregate([
+					  {$unwind:"$comentaris"},
+					  {
+						  "$group": {
+							   "_id": "$id_usuari",
+							   "text_comentaris":{$push:"$comentaris.text_comentari"}
+						   }
+					  },
+					  {
+						  "$project": {
+							   "_id":false,
+							   "id_usuari": "$_id",
+							   "text_comentaris": "$text_comentaris"
+						   }
+					  },
+					  {
+						  "$sort": { "id_usuari": 1}
+					  }
+])
+
 
 // Retweets que té cada usuari 
 db.tweets.aggregate([
@@ -95,18 +130,75 @@ db.tweets.aggregate([
 					  {
 						  "$group": {
 							   "_id": "$id_usuari",
-							   "retweets": {"$sum": 1}
+							   "text_retweets":{$push:"$retweets.text_retweet"}
 						   }
 					  },
 					  {
 						  "$project": {
-							   "_id": false,
-							   "usuari": "$_id",
-							   "retweets": "$retweets"
-						  }
+							   "_id":false,
+							   "id_usuari": "$_id",
+							   "text_retweets": "$text_retweets"
+						   }
 					  },
 					  {
-						  "$sort":{"retweets":-1}
+						  "$sort": { "id_usuari": 1}
 					  }
 ])
 
+
+// Anàlisi execució
+db.tweets.find({"text_tweet":/500/i},{"_id":1,"text_tweet":1,"id_usuari":1,"hashtags.text":1,"likes.usuari_like":1,"comentaris.usuari_comentari":1,"comentaris.text_comentari":1}).explain("executionStats")
+{
+	"cursor" : "BasicCursor",
+	"isMultiKey" : false,
+	"n" : 33974,
+	"nscannedObjects" : 6000000,
+	"nscanned" : 6000000,
+	"nscannedObjectsAllPlans" : 6000000,
+	"nscannedAllPlans" : 6000000,
+	"scanAndOrder" : false,
+	"indexOnly" : false,
+	"nYields" : 68050,
+	"nChunkSkips" : 0,
+	"millis" : 8207,
+	"allPlans" : [
+		{
+			"cursor" : "BasicCursor",
+			"isMultiKey" : false,
+			"n" : 33974,
+			"nscannedObjects" : 6000000,
+			"nscanned" : 6000000,
+			"scanAndOrder" : false,
+			"indexOnly" : false,
+			"nChunkSkips" : 0
+		}
+	],
+	"server" : "mongotwitter:27017",
+	"filterSet" : false,
+	"stats" : {
+		"type" : "PROJECTION",
+		"works" : 6024447,
+		"yields" : 68050,
+		"unyields" : 68050,
+		"invalidates" : 0,
+		"advanced" : 33974,
+		"needTime" : 0,
+		"needFetch" : 24445,
+		"isEOF" : 1,
+		"children" : [
+			{
+				"type" : "COLLSCAN",
+				"works" : 6024447,
+				"yields" : 68050,
+				"unyields" : 68050,
+				"invalidates" : 0,
+				"advanced" : 33974,
+				"needTime" : 5966027,
+				"needFetch" : 0,
+				"isEOF" : 1,
+				"docsTested" : 6000000,
+				"children" : [ ]
+			}
+		]
+	}
+}
