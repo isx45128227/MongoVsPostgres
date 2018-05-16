24495cb5aa631c7e89d7796bf6bd4f133364e1e4
isx45128227 <roogeer96@gmail.com>
2018-05-16 10:42:33 +0200

    Join mongo

---
+++ MongoDB/funcio_join.js
@@ -0,0 +1,34 @@
+function join_tweets_usuaris(query) {
+
+  var data_inicial = new Date();
+  var hora_inicial = data_inicial;
+  
+  var result = db.tweets.find(query);
+  var sortida;
+  
+  while ( result.hasNext()) {
+    
+     var usuari = tojson (result.next().id_usuari);
+     
+     var info_usuari = db.users.find({"id_usuari":parseInt(usuari)});
+     
+     var dades_usuari = info_usuari.next();
+     
+     
+     if (dades_usuari.seguidors){
+     
+        sortida = sortida + " | " + usuari+" "+dades_usuari.nom + " " + dades_usuari.seguidors.length;
+     }
+     else
+     {
+		 sortida = sortida + " | " + usuari+" "+dades_usuari.nom + "  0";
+	}
+     var data_final = new Date();
+     var hora_final = data_final;
+     
+     var total = data_final.getTime()-data_inicial.getTime();
+     }
+  
+  printjson(data_inicial+' Total: '+total + " " + sortida);
+
+}
