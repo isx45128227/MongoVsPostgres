df879968f9f0b809623d00477cc17381437890fc
isx45128227 <roogeer96@gmail.com>
2018-05-16 10:43:00 +0200

    Atac Postgres

--- Postgres/Atac/consulta_atac.sql
+++ Postgres/Atac/consulta_atac.sql
@@ -1,18 +1,14 @@
 -- Consulta que es realitzara amb script d'atac a Postgres
 
-SELECT DISTINCT tweets.id_tweet AS "id_tweet", 
-tweets.text_tweet AS "text_tweet",
- tweets.id_usuari AS "user_tweet", 
- usuaris.nom AS "nom_user" ,
- usuaris.username AS "username" ,
- hashtags.hashtag AS "hashtag_used" ,
- likes.id_usuari_like AS "user_like", 
- comentaris.id_usuari_comentari AS "user_comment", comentaris.text_comentari 
+SELECT tweets.id_tweet AS "id_tweet", tweets.text_tweet AS "text_tweet", 
+   tweets.id_usuari AS "user_tweet", usuaris.nom AS "nom_user" ,usuaris.username AS "username" ,
+   hashtags.hashtag AS "hashtag_used" ,count(likes.id_usuari_like) AS "user_like", 
+   count(comentaris.id_usuari_comentari) AS "user_comment", count(seguidors.id_usuari_seguidor) 
 FROM tweets 
    JOIN usuaris ON tweets.id_usuari=usuaris.id_usuari 
-   JOIN hashtagstweets ON tweets.id_tweet=hashtagstweets.id_tweet 
-   JOIN seguidors ON usuaris.id_usuari=seguidors.id_usuari_seguit
-   JOIN hashtags ON hashtagstweets.id_hashtag=hashtags.id_hashtag
-   JOIN likes ON tweets.id_tweet=likes.id_tweet
-   JOIN comentaris ON tweets.id_tweet=comentaris.id_tweet 
-   WHERE text_tweet LIKE '%#chip%';
+   LEFT JOIN hashtagstweets ON tweets.id_tweet=hashtagstweets.id_tweet 
+   LEFT JOIN seguidors ON usuaris.id_usuari=seguidors.id_usuari_seguit
+   LEFT JOIN hashtags ON hashtagstweets.id_hashtag=hashtags.id_hashtag
+   LEFT JOIN likes ON tweets.id_tweet=likes.id_tweet
+   LEFT JOIN comentaris ON tweets.id_tweet=comentaris.id_tweet 
+   WHERE text_tweet LIKE '%#chip%' GROUP BY tweets.id_tweet,usuaris.id_usuari,hashtags.id_hashtag;
